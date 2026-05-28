8e5a594bf2a286506f88cfeb3f5dfa35da7eb389
전명훈 <acekid2120@gmail.com>
2026-05-28 10:11:26 +0900

    강의 실습

---
+++ SpringAsyncExceptionDemo/src/main/java/com/codeit/springasyncexceptiondemo/SpringAsyncExceptionDemoApplication.java
@@ -0,0 +1,20 @@
+package com.codeit.springasyncexceptiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+@EnableRetry
+public class SpringAsyncExceptionDemoApplication {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(
+        SpringAsyncExceptionDemoApplication.class,
+        args
+    );
+  }
+}
